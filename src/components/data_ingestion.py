f7487d339346819212224b030b8d23c0bde9c0ca
Abhik Ghosh <abhik2018ghosh@gmail.com>
2026-01-30 01:16:20 +0530

    model.pkl file completion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -62,6 +62,10 @@ if __name__=="__main__":
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
-   
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
